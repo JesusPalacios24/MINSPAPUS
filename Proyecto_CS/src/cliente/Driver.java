d8621316c662807ae19f2dc1130dd7d1cddceb77
KYADMZ <118216927+KYADMZ@users.noreply.github.com>
2024-03-16 17:34:30 -0600

    driver jijija

--- Proyecto_CS/src/cliente/Driver.java
+++ Proyecto_CS/src/cliente/Driver.java
@@ -5,10 +5,51 @@
  */
 package cliente;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author mazuk
  */
 public class Driver {
     
+      Socket conexion;         
+         String solicitud = "",respuesta="";
+         int caracter;
+         InputStream entrada;
+         OutputStream salida;
+         
+    public static void main(String[] args) {
+        
+        Driver miDriver = new Driver("localhost",3000);
+        
+        double result;
+        result = miDriver.suma(4.5,8);
+        System.out.println("la suma fue: "+result);
+        miDriver.Desconectar();
+        miDriver = new DriverCalc("localhost",3000);
+        
+        result = miDriver.resta(100,10000);
+        System.out.println("la resta fue: "+result);
+        
+        miDriver.Desconectar();
+        
+        
+    }
+    
+    public String Conexion(usuario,contraseña,puerto){
+        
+    }
+    
+    public void Desconectar(){
+            try{
+            conexion.close();
+            entrada.close();
+            salida.close();
+            }catch(Exception e){
+                System.out.println("Error: "+e.getMessage());
+            }
+   }
 }
