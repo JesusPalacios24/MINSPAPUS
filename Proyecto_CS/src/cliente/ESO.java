9a8c44cebf7b1e6e990b0ad74e8b9b7c39c66bc7
KYADMZ <118216927+KYADMZ@users.noreply.github.com>
2024-03-16 17:03:59 -0600

    importo

--- Proyecto_CS/src/cliente/ESO.java
+++ Proyecto_CS/src/cliente/ESO.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package cliente;
 
-/**
- *
- * @author jpala
- */
+package cliente;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ESO {
+    
+   
+   
+    
     
 }
